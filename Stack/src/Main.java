55f2d87a5a19167d365b6765b70d94b282743290
JayceQ <16831024@qq.com>
2020-04-21 21:49:05 +0800

    Used LinkedList implements a stack, time complexity O(n).

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,5 +12,38 @@ public class Main {
         stack.pop();
 
         System.out.println(stack);
+        LinkedListStack<Integer> linkedListStack= new LinkedListStack<>();
+        for (int i = 0; i < 5; i++) {
+            linkedListStack.push(i);
+        }
+        System.out.println(linkedListStack);
+
+        stack.pop();
+
+        System.out.println(linkedListStack);
+
+        int opCount = 100000;
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        System.out.println("ArrayStack, time: "+testStack(arrayStack,opCount)+"ms");
+
+        LinkedListStack<Integer> linkedListStack1 = new LinkedListStack<>();
+        System.out.println("LinkedListStack, time: "+testStack(linkedListStack1,opCount)+"ms");
+
+
+    }
+
+    public static double testStack(Stack<Integer> stack,int opCount){
+        long start = System.nanoTime();
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            stack.push(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < opCount; i++) {
+            stack.pop();
+        }
+
+        long end = System.nanoTime();
+
+        return (end - start) / 1000000.0;
     }
 }
