7945560423340d1aad20b830f49d37c757f3ce4d
JayceQ <16831024@qq.com>
2020-06-13 11:08:12 +0800

    动态扩容的HashTable

---
+++ HashCode/src/HashTable.java
@@ -0,0 +1,93 @@
+import java.util.TreeMap;
+
+public class HashTable<K extends Comparable<K>, V> {
+
+    //素数表
+    private static final int[] capacity = {53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593,
+            49157, 98317, 196613, 393241, 786433, 1572869, 3145739, 6291469,
+            12582917, 25165843, 50331653, 100663319, 201326611, 402653189, 805306457, 1610612741};
+    private static final int upperTol = 10;
+    private static final int lowerTol = 2;
+    private int capacityIndex = 0;
+
+    private TreeMap[] hashTable;
+    private int M;
+    private int size;
+
+    public HashTable() {
+        this.M = capacity[capacityIndex];
+        size = 0;
+        hashTable = new TreeMap[M];
+        for (int i = 0; i < M; i++) {
+            hashTable[i] = new TreeMap<>();
+
+        }
+    }
+
+    private int hash(K key) {
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(K key, V value) {
+        TreeMap<K, V> map = hashTable[hash(key)];
+        if (map.containsKey(key)) {
+            map.put(key, value);
+        } else {
+            map.put(key, value);
+            size++;
+            if (size >= upperTol * M && capacityIndex + 1 < capacity.length)
+                capacityIndex++;
+            resize(capacity[capacityIndex]);
+        }
+    }
+
+    private void resize(int newM) {
+        TreeMap<K, V>[] newHashTable = new TreeMap[newM];
+        for (int i = 0; i < newM; i++) {
+            newHashTable[i] = new TreeMap<>();
+        }
+        int oldM = M;
+        this.M = newM;
+        for (int i = 0; i < oldM; i++) {
+            TreeMap<K, V> map = hashTable[i];
+            for (K k : map.keySet()) {
+                newHashTable[hash(k)].put(k, map.get(k));
+            }
+        }
+        this.hashTable = newHashTable;
+    }
+
+    public V remove(K key) {
+        TreeMap<K, V> map = hashTable[hash(key)];
+        V ret = null;
+        if (map.containsKey(key)) {
+            ret = map.remove(key);
+            size--;
+            if (size < lowerTol * M && capacityIndex > 0) {
+                capacityIndex--;
+                resize(capacity[capacityIndex]);
+            }
+        }
+        return ret;
+    }
+
+    public void set(K key, V value) {
+        TreeMap<K, V> map = hashTable[hash(key)];
+        if (!map.containsKey(key)) {
+            throw new IllegalArgumentException(key + " doesn't exist!");
+        }
+        map.put(key, value);
+    }
+
+    public boolean contains(K key) {
+        return hashTable[hash(key)].containsKey(key);
+    }
+
+    public V get(K key) {
+        return (V) hashTable[hash(key)].get(key);
+    }
+}
